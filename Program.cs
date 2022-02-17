bed291061c624cf2575bb06029be6613313f7d6c
Kyle Evans <krevan88@protonmail.com>
2022-02-17 15:10:46 -0700

    initcommit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//run program, take input, math.random a selection, compare the outcome to the input, match type = draw, define non-matches...
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Clear();
+Console.WriteLine("Let's play Rock, Paper, Scissors!");
+Console.WriteLine();
+Console.WriteLine("Please make your selection...");
+var weapon = Console.ReadLine();
+Console.WriteLine($"You have selected {weapon}");
